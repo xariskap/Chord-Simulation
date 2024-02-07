08fd284f88c6d5510cb3c08466fb8b416988c2ef
Xaris Kap <xariskapelet@gmail.com>
2024-02-07 14:26:19 +0200

    fix

--- main.go
+++ main.go
@@ -15,15 +15,18 @@ func main() {
 	for _, ip := range ipArray {
 		node := chord.NewNode(ip)
 		ring.Join(&node)
-	}
 
-	for _, node := range(ring.Nodes){
-		node.FixFingers()
-	}
 
-	ring.String()
+		if len(ring.Nodes)%9 == 0 {
+			for _, node := range ring.Nodes {
+				node.FixFingers()
+			}}
+		}
+	
+		ring.String()
 
-	for _,v := range(ring.Nodes[6].FingerTable){
+	for _,v := range(ring.Nodes[5].FingerTable){
 		fmt.Println(v.Start, v.Successor.Id)
 	}
+
 }
